08289ee64b76a2d4f8fb0ae69bcc852ef092a2a9
Artem <aprytkov@gmail.com>
2019-01-24 07:14:23 +0300

    Работа над заданием 6

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,63 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+import math
+class Triangle():
+    def __init__(self,a, b, c):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._ab = self._side_length(a, b)
+        self._ac = self._side_length(a, c)
+        self._bc = self._side_length(b, c)
+
+    def _side_length(self,a, b):
+        return round(math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2), 2)
+
+    def area(self):
+        """Расчет площади треугольника по формуле Герона"""
+        s = (self._ab + self._ac + self._bc) / 2
+        area = math.sqrt(s * (s - self._ab) * (s - self._ac)* (s - self._bc))
+        return round(area, 2)
+
+    def heights(self):
+        """Возвращает массив высот треугольника"""
+        return[round(self.area() / base, 2) 
+            for base in [self._ab, self._ac, self._bc]]
+
+    def perimeter(self):
+        return round(self._ac + self._ab + self._bc, 2)
+
+
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+
+class Trapeze():
+    def __init__(self, a, b, c, d):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
+        self._ab = self._side_length(a, b)
+        self._ac = self._side_length(a, c)
+        self._bc = self._side_length(b, c)
+        self._cd = self._side_length(c, d)
+
+    def _side_length(self,a, b):
+        return round(math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2), 2)
+
+    def sides():
+        return [self._ab, self._ac, self._bc, self._cd]
+
+    def perimeter(self):
+        return round(sum(self.sides()), 2)
+
+if __name__ == '__main__':
+    tr = Triangle([0,0], [3, 6], [6,6])
+    print(tr.area())
+    print(tr.heights())
+    print(tr.perimeter())
 
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,113 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+import itertools
+
+class School():
+    def __init__(self, classes=[], teachers=[]):
+        self._classes = classes
+        self._teachers = teachers
+
+    def classes(self):
+        return self._classes
+
+    def class_list(self):
+        return [cl.name for cl in self._classes]
+
+    def teachers(self):
+        return self._teachers
+
+    def students_for_class(self, class_name):
+        classes = filter(lambda x: x.name == class_name, self._classes)
+        students = itertools.chain.from_iterable( [cl.students for cl in classes])
+        return [st.name() for st in students]
+
+    def teachers_for_class(self, class_name):
+        cl = self._find_class_by_name(class_name)
+        teachers = [t.name() for t in self._teachers if cl in t.classes()]
+        return teachers
+
+    def student_courses(self, student_name):
+        # Ученик --> Класс --> Учителя --> Предметы
+        student = _self._find_student_by_name
+        student_classes = [cl for cl in self._classes if student in cl.students]
+        pass
+
+    def student_parents(self, student_name):
+        student = self._find_student_by_name(name)
+        return student.parent_names()
+
+    def _find_class_by_name(self, name):
+        cl = [cl for cl in self._classes if cl.name == name]
+        return cl[0]
+
+    def _find_student_by_name(self, name):
+        students = list(itertools.chain.from_iterable( [cl.students for cl in self._classes]))
+        # print([st.name() for st in students])
+        student = list(filter(lambda x: x.name() == student_name, students))[0]
+        return student
+
+
+class SchoolClass():
+    def __init__(self, name, students=[]):
+        self.name = name
+        self.students = students
+
+    def __str__(self):
+        return "%s" % self.name
+
+class Person(object):
+    """docstring for Person"""
+    def __init__(self, name):
+        self._name = name
+
+    def name(self):
+        return self._name        
+
+class Teacher(Person):
+    def __init__(self, name, course, classes):
+        super(Teacher, self).__init__(name)
+        self._course = course
+        self._classes = classes
+
+    def classes(self):
+        return self._classes
+
+class Student(Person):
+    """docstring for Student"""
+    def __init__(self, name, father, mother):
+        super(Student, self).__init__(name)
+        self._father = father
+        self._mother = mother
+
+    def parent_names(self):
+        return [self._father.name(), self._mother.name()]
+
+    def __str__(self):
+        return "%s" % self.name
+
+def build_data():
+    student1 = Student("Михеев И.Л.", Person("Михеев Л. Д."), Person("Михеева А. И."))
+    student2 = Student("Рыбаков М.Б.", Person("Рыбаков Б. Т."), Person("Рыбакова Н.Ф."))
+    student3 = Student("Горбунова А.Я.", Person("Горбунов Я.М."), Person("Горбунова Н.И."))
+
+    class1 = SchoolClass("4A", [student1, student2])
+    class2 = SchoolClass("4Б", [student3])
+    class3 = SchoolClass("4В", [])
+
+    teacher1 = Teacher("Иванов И.И.", "русский", [class1, class2])
+    teacher2 = Teacher("Петрова П.П.", "математика", [class1, class2])
+
+    school = School([class1, class2, class3], [teacher1, teacher2])
+
+    return school
+
+
+if __name__ == '__main__':
+    school = build_data()
+    print("Список классов:", school.class_list()) # 1
+    print("Ученики в классе:", school.students_for_class("4A")) # 2
+    print("Предметы ученика:", school.student_courses("Горбунова А.Я.")) #3
+    print("Родители ученика:", school.student_parents("Михеев И.Л.")) # 4
+    print("Учителя в классе:", school.teachers_for_class("4Б")) # 5
